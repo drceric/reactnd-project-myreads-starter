42bf0f3b9a60de18ee9c82aadfceebd24f948257
Eric Liu <drc.ericlau@gmail.com>
2017-07-24 20:10:08 -0700

    feat: Go back and add a book button works now

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,22 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: true,
+    books: []
+  }
+
+  onClickToMyBook = () => {
+    this.setState({ showSearchPage: false})
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <Searchbar />
+          <Searchbar
+            onGoBack={this.onClickToMyBook}
+            page={this.state.showSearchPage}
+          />
         ) : (
           <div className="list-books">
             <div className="list-books-title">
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,13 +3,28 @@ import PropTypes from 'prop-types'
 import Bookgrid from './Bookgrid.js'
 
 class Searchbar extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
   render() {
+    const { onGoBack, page } = this.props
+    const { query } = this.state
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <a className="close-search" onClick={() => onGoBack()}>Close</a>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
